230e79de1c6352f79d0ed47c4ff55244f80f08aa
Christian Rauch <Christian.Rauch@ed.ac.uk>
2016-06-02 19:28:38 +0100

    use aiPrimitiveType_TRIANGLE to find first triangle mesh in scene

--- src/mesh/assimp_mesh_reader.cpp
+++ src/mesh/assimp_mesh_reader.cpp
@@ -11,36 +11,40 @@ namespace dart {
 dart::Mesh * AssimpMeshReader::readMesh(const std::string filename) const {
 
     const struct aiScene * scene = aiImportFile(filename.c_str(),0); //aiProcess_JoinIdenticalVertices);
+
     if (scene == 0) {
         std::cerr << "error: " << aiGetErrorString() << std::endl;
-        return 0;
-    }
-
-    if (scene->mNumMeshes != 1) {
-        std::cerr << "there are " << scene->mNumMeshes << " meshes in " << filename << std::endl;
         aiReleaseImport(scene);
         return 0;
     }
 
-    aiMesh * aiMesh = scene->mMeshes[0];
-
-    dart::Mesh * mesh = new dart::Mesh(aiMesh->mNumVertices,aiMesh->mNumFaces);
-
-    for (int f=0; f<mesh->nFaces; ++f) {
-        aiFace& face = aiMesh->mFaces[f];
-        if (face.mNumIndices != 3) {
-            std::cerr << filename << " is not a triangle mesh" << std::endl;
-            delete mesh;
-            aiReleaseImport(scene);
-            return 0;
-        }
-        mesh->faces[f] = make_int3(face.mIndices[0],face.mIndices[1],face.mIndices[2]);
+    // find first triangle mesh
+    aiMesh * aiMesh = NULL;
+    // stop searching if end of list or if first mesh is found (aiMesh!=NULL)
+    for(unsigned int i = 0; i<scene->mNumMeshes && aiMesh==NULL; i++) {
+        if(scene->mMeshes[i]->mPrimitiveTypes == aiPrimitiveType_TRIANGLE)
+            aiMesh = scene->mMeshes[i];
+        else
+            std::cout<<"ignoring non-triangle mesh in "<<filename<<std::endl;
     }
-    for (int v=0; v<mesh->nVertices; ++v) {
-        aiVector3D & vertex = aiMesh->mVertices[v];
-        mesh->vertices[v] = make_float3(vertex.x,vertex.y,vertex.z);
-        aiVector3D & normal = aiMesh->mNormals[v];
-        mesh->normals[v] = make_float3(normal.x,normal.y,normal.z);
+
+    dart::Mesh * mesh = NULL;
+    if(aiMesh != NULL) {
+        mesh = new dart::Mesh(aiMesh->mNumVertices,aiMesh->mNumFaces);
+
+        for (int f=0; f<mesh->nFaces; ++f) {
+            aiFace& face = aiMesh->mFaces[f];
+            mesh->faces[f] = make_int3(face.mIndices[0],face.mIndices[1],face.mIndices[2]);
+        }
+        for (int v=0; v<mesh->nVertices; ++v) {
+            aiVector3D & vertex = aiMesh->mVertices[v];
+            mesh->vertices[v] = make_float3(vertex.x,vertex.y,vertex.z);
+            aiVector3D & normal = aiMesh->mNormals[v];
+            mesh->normals[v] = make_float3(normal.x,normal.y,normal.z);
+        }
+    } // if aiMesh!=NULL
+    else {
+        std::cerr << "there are no triangle meshes in " << filename << std::endl;
     }
 
     aiReleaseImport(scene);
