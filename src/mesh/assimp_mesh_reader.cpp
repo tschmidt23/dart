d42079c06247fb56c73d01bd75247b11f17599b8
Christian Rauch <Christian.Rauch@ed.ac.uk>
2016-08-23 18:21:52 +0100

    support collada meshes (*.dae)
    The kinematic tree in the mesh is traversed recursively to obtain all
    vertices, normals and faces and their transformation into the root node
    frame. For mehses without tree structure (e.g. obj) there is only one node
    which is the root node.

--- src/mesh/assimp_mesh_reader.cpp
+++ src/mesh/assimp_mesh_reader.cpp
@@ -7,16 +7,80 @@
 #include <vector_types.h>
 #include <vector_functions.h>
 
+#include <assert.h>
+
 namespace dart {
+
+template<typename T>
+size_t vec_byte_size(const typename std::vector<T>& vec) {
+    return sizeof(T) * vec.size();
+}
+
+aiMatrix4x4 getFullT(const std::vector<aiMatrix4x4> &transforms) {
+    // identity transform
+    aiMatrix4x4 T;
+    // concatenate all transformations
+    for(auto t : transforms)
+        T *= t;
+    return T;
+}
+
+void getMesh(const aiScene* const scene, const aiNode* const node,
+             std::vector<float3> &verts, std::vector<float3> &normals, std::vector<int3> &faces, std::vector<aiMatrix4x4> transforms)
+{
+    // start to count mesh ID from here
+    const unsigned int offset = verts.size();
+
+    // we need to skip the transformation from the scene to the root frame
+    if(node->mParent != NULL)
+        transforms.push_back(node->mTransformation);
+
+    const aiMatrix4x4 T = getFullT(transforms);
+
+    for(unsigned int m=0; m<node->mNumMeshes; m++) {
+        const aiMesh* const mesh = scene->mMeshes[node->mMeshes[m]];
+        if(mesh->mPrimitiveTypes == aiPrimitiveType_TRIANGLE) {
+            // transform each vertice
+            for(unsigned int v=0; v<mesh->mNumVertices; v++) {
+                const aiVector3D vert = T*mesh->mVertices[v];
+                verts.push_back(make_float3(vert.x, vert.y, vert.z));
+            }
+
+            // transform each normal
+            for(unsigned int v=0; v<mesh->mNumVertices && mesh->HasNormals(); v++) {
+                const aiVector3D norm = T*mesh->mNormals[v];
+                normals.push_back(make_float3(norm.x, norm.y, norm.z));
+            }
+
+            // get faces and store vertex ID with offsets for each new mesh
+            for(unsigned int f=0; f<mesh->mNumFaces; f++) {
+                const aiFace face = mesh->mFaces[f];
+                assert(face.mNumIndices == 3);
+                faces.push_back(make_int3(offset + face.mIndices[0],
+                                          offset + face.mIndices[1],
+                                          offset + face.mIndices[2]));
+            }
+        } // check aiPrimitiveType_TRIANGLE
+    }
+
+    // recursively for each child node
+    for(unsigned int i=0; i<node->mNumChildren; i++) {
+        const aiNode* const child = node->mChildren[i];
+        getMesh(scene, child, verts, normals, faces, transforms);
+    }
+}
 
 dart::Mesh * AssimpMeshReader::readMesh(const std::string filename) const {
 
     // define import flags for assimp
-    const unsigned int import_flags = aiProcess_Triangulate | aiProcess_SortByPType;
+    const unsigned int import_flags = aiProcess_Triangulate |
+                                      aiProcess_SortByPType |
+                                      aiProcess_JoinIdenticalVertices;
     // aiProcess_Triangulate: triangulate ploygones such that all vertices have 3 points
     // aiProcess_SortByPType: split meshes with mixed types into separate meshes
+    // aiProcess_JoinIdenticalVertices: remove vertices with same coordinates
 
-    const struct aiScene * scene = aiImportFile(filename.c_str(), import_flags); //aiProcess_JoinIdenticalVertices);
+    const struct aiScene * scene = aiImportFile(filename.c_str(), import_flags);
 
     if (scene == 0) {
         std::cerr << "error: " << aiGetErrorString() << std::endl;
@@ -24,48 +88,24 @@ dart::Mesh * AssimpMeshReader::readMesh(const std::string filename) const {
         return 0;
     }
 
-    // find first triangle mesh
-    aiMesh * aiMesh = NULL;
-    // stop searching if end of list or if first mesh is found (aiMesh!=NULL)
-    for(unsigned int i = 0; i<scene->mNumMeshes && aiMesh==NULL; i++) {
-        if(scene->mMeshes[i]->mPrimitiveTypes == aiPrimitiveType_TRIANGLE)
-            aiMesh = scene->mMeshes[i];
-        else
-            std::cout<<"ignoring non-triangle mesh in "<<filename<<std::endl;
-    }
+    std::vector<int3> faces;
+    std::vector<float3> verts;
+    std::vector<float3> normals;
+    std::vector<aiMatrix4x4> transforms;
 
-    dart::Mesh * mesh = NULL;
-    if(aiMesh != NULL) {
-        mesh = new dart::Mesh(aiMesh->mNumVertices,aiMesh->mNumFaces);
+    // recursively obtain vertices, normals and faces from meshes
+    const aiNode* const rnode = scene->mRootNode;
+    getMesh(scene, rnode, verts, normals, faces, transforms);
 
-        for (int f=0; f<mesh->nFaces; ++f) {
-            aiFace& face = aiMesh->mFaces[f];
-            mesh->faces[f] = make_int3(face.mIndices[0],face.mIndices[1],face.mIndices[2]);
-        }
-        for (int v=0; v<mesh->nVertices; ++v) {
-            aiVector3D & vertex = aiMesh->mVertices[v];
-            mesh->vertices[v] = make_float3(vertex.x,vertex.y,vertex.z);
-
-        }
-
-        if(aiMesh->HasNormals()) {
-            for(unsigned int n=0; n<aiMesh->mNumVertices; n++) {
-                aiVector3D & normal = aiMesh->mNormals[n];
-                mesh->normals[n] = make_float3(normal.x,normal.y,normal.z);
-            }
-        }
-        else {
-            std::cout<<"triangle mesh in "<<filename<<" has no normals"<<std::endl;
-        }
-    } // if aiMesh!=NULL
-    else {
-        std::cerr << "there are no triangle meshes in " << filename << std::endl;
-    }
+    // copy mesh  data to DART mesh
+    dart::Mesh* mesh = new dart::Mesh(verts.size(), faces.size());
+    memcpy(mesh->vertices, verts.data(), vec_byte_size(verts));
+    memcpy(mesh->normals, normals.data(), vec_byte_size(normals));
+    memcpy(mesh->faces, faces.data(), vec_byte_size(faces));
 
     aiReleaseImport(scene);
 
     return mesh;
-
 }
 
 }
